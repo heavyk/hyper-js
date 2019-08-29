253acfcaf296452fca00872d6ad8dd0226312c7b
flames of love <mechanicofthesequence@gmail.com>
2019-08-29 15:08:56 -0600

    whammy needs a frame

--- components/whammy.js
+++ components/whammy.js
@@ -3,12 +3,13 @@ import { eases } from './toast-zone'
 
 import './whammy.css'
 
-export default function whammy (G) {
+export default function whammy (G, frame) {
   const {h} = G
   return (msg, seconds = 2, ease = 'fast', animation = 'whammy') => {
     let el = h('.whammy', {
       style: { animation: `${animation} ${seconds}s ${eases[ease] || ease} 1 normal forwards` }
     }, msg)
+
     frame.aC(el)
 
     if (seconds) setTimeout(() => {
