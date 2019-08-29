cc0cd0b028cd9cfa0e2871f74997cb6ae6ae299f
flames of love <mechanicofthesequence@gmail.com>
2019-08-29 15:09:10 -0600

    debug obvs

--- dom/observables.js
+++ dom/observables.js
@@ -6,3 +6,13 @@
 //   numeric attributes (cept for opacity)
 export var _px = (v) => typeof v === 'string' && ~v.indexOf('px') ? v : v + 'px'
 export var px = (obv) => transform(obv, _px)
+
+// log a value to the console
+export var obv_log = (obv, name = obv._obv, level = 'log') => {
+  return obv((v) => console[level](name+':', v))
+}
+
+export var obv_debug = (obv, ns = '') => {
+  let log = require('debug')(ns)
+  return obv((v) => log(name+':', v))
+}
