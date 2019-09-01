bbfdcafca2cc88973cfc160e6594fe7c38d5625c
flames of love <mechanicofthesequence@gmail.com>
2019-09-01 09:50:57 -0600

    toasts feel a little better now

--- components/toast-zone.js
+++ components/toast-zone.js
@@ -1,4 +1,6 @@
 
+import { each } from '@hyper/utils'
+
 import './toast-zone.css'
 
 export const eases = {
@@ -10,21 +12,34 @@ export default function toaster (G, toast_zone_style) {
   const {h} = G
 
   let toast_zone = h('.toast-zone', {style: toast_zone_style})
+  let toasts = []
+
+  function remove (el) {
+    let idx = toasts.indexOf(el)
+    if (~idx) {
+      toasts.splice(idx, 1)
+      el.style.opacity = 0
+      setTimeout(() => toast_zone.rC(el), 1000) // max 1s fade out... this isn't the 90's :)
+    }
+  }
+
   toast_zone.toast = (msg, seconds = 3, ease = 'ease-in', animation = 'toasted') => {
 
     let el = h('.toast', {
-      boink: function () { this.style.display = 'none' },
+      boink: () => { el.style.display = 'none' },
       style: {animation: `${animation} .1s ${eases[ease] || ease}`}
     }, msg)
 
     toast_zone.aC(el)
+    toasts.push(el)
 
-    if (seconds) setTimeout(() => {
-      el.style.opacity = 0
-      setTimeout(() => toast_zone.rC(el), 1000) // max 1s fade out... this isn't the 90's :)
-    }, seconds * 1000)
+    if (seconds) setTimeout(remove, seconds * 1000, el)
 
     return el
+  }
+
+  toast_zone.clear = () => {
+    each(toasts, remove)
   }
 
   return toast_zone
