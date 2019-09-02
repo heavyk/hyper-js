c2c8cdace8b9f7199914ec74679b6196552dfe75
flames of love <mechanicofthesequence@gmail.com>
2019-09-02 12:04:01 -0600

    modal close button configurable

--- components/modal.js
+++ components/modal.js
@@ -9,6 +9,7 @@ export default function modal (G, opts = {}) {
   opts.title = v(opts.title || null) // null so that it gets a value. if it remains undefined, the obv won't init.
   opts.content = v(opts.content(ctx))
   opts.footer = v(opts.footer)
+  let no_close_button = opts.close_button == 0
   let close = opts.close = () => {
     el.rm()
     ctx.cleanup()
@@ -19,14 +20,17 @@ export default function modal (G, opts = {}) {
   // @Incomplete: switch the close button from text content to use the i.close element instead
 
   let el =
-  h('.modal-background', {boink: (ev) => { ev.target === el && close() }},
+  h('.modal-background', {boink: (ev) => { ev.target === el && !opts.no_background_close && close() }},
     h('.modal',
       t(opts.title, (title) => title ?
         h('h1.header', opts.title,
-          h('.modal-close', {boink: opts.close}, h('i.close'))
+          no_close_button ? null : {style: {paddingRight: '40px'}},
+          no_close_button ? null :
+            h('.modal-close', {boink: opts.close}, h('i.close'))
         ) :
         h('.headerless',
-          h('.modal-close', {boink: opts.close}, h('i.close'))
+          no_close_button ? null :
+            h('.modal-close', {boink: opts.close}, h('i.close'))
         )
       ),
       h('.modal-content', opts.content),
