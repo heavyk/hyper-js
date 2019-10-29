855cff587aabc9db594517bb6345a8bf1946315c
flames of love <mechanicofthesequence@gmail.com>
2019-10-29 14:52:55 -0600

    add the concept of pinning toasts

--- components/toast-zone.js
+++ components/toast-zone.js
@@ -1,6 +1,4 @@
 
-import { each } from '@hyper/utils'
-
 import './toast-zone.css'
 
 export const eases = {
@@ -14,17 +12,24 @@ export default function toaster (G, toast_zone_style) {
   let toast_zone = h('.toast-zone', {style: toast_zone_style})
   let toasts = []
 
-  function remove (el) {
+  function remove (el, keep_pinned) {
     let idx = toasts.indexOf(el)
-    if (~idx) {
+    if (~idx && el.pinned != keep_pinned) {
       toasts.splice(idx, 1)
       el.style.opacity = 0
       setTimeout(() => toast_zone.rC(el), 1000) // max 1s fade out... this isn't the 90's :)
     }
   }
 
-  toast_zone.toast = (msg, seconds = 3, ease = 'ease-in', animation = 'toasted') => {
+  toast_zone.toast = (msg, seconds = 3, options = {}) => {
+    let { pinned, ease, animation } = Object.assign({
+      pinned: seconds == 0,
+      ease: 'ease-in',
+      animation: 'toasted',
+    }, options)
+
     let el = h('.toast', {
+      pinned,
       boink: () => { el.style.display = 'none' },
       style: {animation: `${animation} .1s ${eases[ease] || ease}`}
     }, msg)
@@ -39,7 +44,7 @@ export default function toaster (G, toast_zone_style) {
 
   toast_zone.remove = remove
   toast_zone.clear = () => {
-    each(toasts, remove)
+    toasts.forEach((el) => remove(el, true))
   }
 
   return toast_zone
