314d7f8079b4a43c48c2604d7cd4d52e3c9b05ec
flames of love <mechanicofthesequence@gmail.com>
2020-01-14 09:39:04 -0700

    tons of roadtrip improvements.

--- roadtrip.js
+++ roadtrip.js
@@ -7,13 +7,27 @@
 
 import { win, location, origin, base_path } from '@hyper/dom/dom-base'
 import { href_pathname, href_query, href_hash } from '@hyper/dom/dom-base'
+import { lookup_parent_with_attr } from '@hyper/dom/dom-base'
 import { on, off, prevent_default } from '@hyper/dom/dom-base'
-import { noop, slasher, which } from '@hyper/utils'
+import { noop, slasher, which, error } from '@hyper/utils'
 import isEqual from '@hyper/isEqual'
 
 const QUERYPAIR_REGEX = /^([\w\-]+)(?:=([^&]*))?$/
 const HANDLERS = [ 'beforeenter', 'enter', 'leave', 'update' ]
 
+function get_internal_link_element (event) {
+  let el = event.composed ? event.composedPath()[0] : event.target
+  while (el && el.nodeName !== 'A') el = el.parentNode
+
+  return (
+      !el ||
+      el.hasAttribute('download') ||
+      el.getAttribute('rel') === 'external' ||
+      ~el.href.indexOf('mailto:') ||
+      el.target ||
+      !sameOrigin(el.href) // TODO: x-origin navigation function (which can do tracking or cancel the event)
+    ) ? null : el
+}
 
 // @Cleanup: why is this necessary?? seems like the same data structure could be made more succinctly
 class RouteData {
@@ -60,7 +74,7 @@ class Route {
   }
 
   set path (_path) {
-    var path = slasher(_path, 1)
+    let path = slasher(_path, 1)
     this._path = path
     this.segments = path.split('/')
   }
@@ -74,14 +88,13 @@ class Route {
   }
 
   exec (target, isInitial, is404) {
-    const href = target.href
-    const pathname = slasher(href_pathname(href), 1)
-    const segments = pathname.split('/')
-    const _segments = this.segments
-    const params = {}
+    let href = target.href
+    let pathname = slasher(href_pathname(href), 1)
+    let segments = pathname.split('/')
+    let _segments = this.segments
+    let params = {}
 
     if (!is404) {
-      // if (segments.length !== _segments.length) {
       if (segments.length > _segments.length) {
         return false
       }
@@ -92,10 +105,6 @@ class Route {
 
         if (segment_to_match[0] === ':') {
           matches = /^:([\w\-]+)?\?/.exec(segment_to_match)
-          // let len = toMatch.length
-          // if (toMatch.splice(-1) === '?') {
-          //   params[toMatch.slice(1)] = segment
-          // }
           params[matches[1]] = segment
         } else if (segment !== segment_to_match) {
           return false
@@ -161,22 +170,30 @@ const isSameRoute = (routeA, routeB, dataA, dataB) => (
 )
 
 export default class RoadTrip {
-  constructor (base = '') {
-    this.routes = []
-    this.isTransitioning = null
-    this.scrollHistory = {}
-    this.currentData = {}
-    this.currentRoute = {
+  constructor (base = '', container_el) {
+    let self = this
+    self.routes = []
+    self.transitioning = null
+    self.uniqueID = self.currentID = 1
+    self.scrollHistory = {}
+    self.cur_data = {}
+    self.cur_route = {
       enter: () => Promise.resolve(),
       leave: () => Promise.resolve()
     }
 
-    this.uniqueID =
-    this.currentID = 1
+    if (base[0] !== '/') error("base must begin with a '/'")
+    let bl = base.length - 1
+    self.base = base[bl] === '/' ? base.substr(0, bl) : base
 
-    if (base[0] !== '/') throw new Error("base must begin with a '/'")
-    var bl = base.length - 1
-    this.base = base[bl] === '/' ? base.substr(0, bl) : base
+    if (!container_el) container_el = win
+    let parent = lookup_parent_with_attr(container_el, 'roadtrip')
+    if (parent) {
+      if (DEBUG) console.info('parent:', parent)
+      error('parent element is already on a roadtrip!')
+    } else {
+      container_el.roadtrip = self
+    }
   }
 
   add (path, options, ctx = this) {
@@ -192,10 +209,11 @@ export default class RoadTrip {
     // next, for all routes which match
     // eg. if the current path is 4 segments long, and the longest route is 2 segments, then we can assume that for /xxx/xxx/yyy/yyy, /xxx/xxx is the base, and /yyy/yyy is the route (where /yyy/yyy matches one of the routes)
 
-    const start_href = location.href
+    // changed to pathname
+    const start_href = location.pathname // location.href
     const href = this.routes.some(route => route.matches(start_href)) ?
       start_href :
-      options.fallback || this.base || '/'
+      options.fallback || this.base
 
     this.initial = true
     return this.goto(href, {
@@ -205,7 +223,7 @@ export default class RoadTrip {
     })
   }
 
-  goto (href, options = {}) {
+  goto (href = '/', options = {}) {
     this.scrollHistory[this.currentID] = {
       x: win.scrollX,
       y: win.scrollY
@@ -216,9 +234,19 @@ export default class RoadTrip {
     }
 
     let target
+    if ((href = slasher(href)) === this.base) {
+      // the base dir should always end with a slash.
+      // if for example the base is '/lala' and it does not have a trailing slash,
+      // instaed of loading '/lala/lib.js', it'll attempt to load '/lib.js'
+      // so, it must be set in the browser history as '/lala/' so it doesn't get
+      // confused.
+      //
+      //               -kenny 12-01-2020
+      href += '/'
+    }
     const promise = new Promise((fulfil, reject) => {
       target = this._target = {
-        href: slasher(href),
+        href,
         scrollX: options.scrollX || 0,
         scrollY: options.scrollY || 0,
         options,
@@ -230,63 +258,64 @@ export default class RoadTrip {
     target.promise = promise
 
     // only if we're not transitioning, will we goto the target
-    if (this.isTransitioning === null) this.goto_target(target)
+    if (this.transitioning === null) this.goto_target(target)
 
     return promise
   }
 
   goto_target (target) {
-    let currentData = this.currentData
-    let currentRoute = this.currentRoute
-    let newRoute
-    let newData
+    let cur_data = this.cur_data
+    let cur_route = this.cur_route
+    let new_route
+    let new_data
     let promise
 
     if (target.options.code === 404) {
-      if (newRoute = this._404) newData = newRoute.exec(target, this.initial, true)
-      else console.error('404: route not found', target.href)
+      if (new_route = this._404) new_data = new_route.exec(target, this.initial, true)
+      else error('no handler for 404 is present')
     } else {
       for (let route of this.routes) {
-        if (newData = route.exec(target, this.initial)) {
-          newRoute = route
+        if (new_data = route.exec(target, this.initial)) {
+          new_route = route
           this.initial = false
           break
         }
       }
 
       // 404's don't replace state
-      if (isSameRoute(newRoute, currentRoute, newData, currentData)) {
+      if (isSameRoute(new_route, cur_route, new_data, cur_data)) {
         target.options.replace = true
       }
     }
 
-    if (!newRoute) {
+    if (!new_route) {
       // this can only happen if a 404 is not defined
-      return this.goto(this.base || '/')
+      return this.goto(this.base + '/')
     }
 
     this.scrollHistory[ this.currentID ] = {
-      x: (currentData.scrollX = win.scrollX),
-      y: (currentData.scrollY = win.scrollY)
+      x: (cur_data.scrollX = win.scrollX),
+      y: (cur_data.scrollY = win.scrollY)
     }
 
-    this.isTransitioning = target
+    this.transitioning = target
 
     promise =
-      newRoute === currentRoute && typeof newRoute.update === 'function' ?
-        newRoute.update(newData, currentData)
-      : currentRoute.leave(currentData, newData)
-        .then(() => newRoute.beforeenter(newData, currentData))
-        .then(() => newRoute.enter(newData, currentData))
+      new_route === cur_route && typeof new_route.update === 'function' ?
+        new_route.update(new_data, cur_data)
+      : cur_route.leave(cur_data, new_data)
+        .then(() => new_route.beforeenter(new_data, cur_data))
+        .then(() => new_route.enter(new_data, cur_data))
         .catch((err) => {
-          console.error('ERROR!', err)
+          // @Incomplete: better error message reporting
+          error(err)
         })
 
     promise
       .then((val) => {
-        this.currentRoute = newRoute
-        this.currentData = newData
-        this.isTransitioning = null
+        this.cur_route = new_route
+        this.cur_data = new_data
+        this.transitioning = null
 
         // if the user navigated while the transition was taking
         // place, we need to do it all again
@@ -304,6 +333,7 @@ export default class RoadTrip {
     if (target.popstate || invisible) return
 
     const uid = replace ? this.currentID : ++this.uniqueID
+    if (DEBUG) console.info(replace ? 'replace' : 'new', 'location:', target.href)
     win.history[ replace ? 'replaceState' : 'pushState' ]({ uid, code }, '', target.href)
 
     this.currentID = uid
@@ -314,79 +344,65 @@ export default class RoadTrip {
 
     return promise
   }
+}
 
-  // Adapted from https://github.com/visionmedia/page.js
-  // MIT license https://github.com/visionmedia/page.js#license
-  // further modification from https://github.com/Rich-Harris/roadtrip/blob/master/src/utils/watchLinks.js
-  //  - added link detection in custom elements
-  //  - allow the link watching to be contained to an element (workaround for document level passive listeners)
+// Adapted from https://github.com/visionmedia/page.js
+// MIT license https://github.com/visionmedia/page.js#license
+// further modification from https://github.com/Rich-Harris/roadtrip/blob/master/src/utils/watchLinks.js
+//  - added link detection in custom elements
+//  - allow the link watching to be contained to an element (workaround for document level passive listeners)
 
-  watchLinks (container_el) {
-    let ev_goto = (event, path, options) => {
-      // preventDefault on document level ecents is no longer possible starting with chrome 56
-      // (all document level event listeners are considered passive by default)
-      // https://www.chromestatus.com/feature/5093566007214080
-      prevent_default(event)
-      return this.goto(path, options)
+export function watchLinks (roadtrip, container_el) {
+  let cancel_event_and_goto = (event, path, options) => {
+    // preventDefault on document level ecents is no longer possible starting with chrome 56
+    // (all document level event listeners are considered passive by default)
+    // https://www.chromestatus.com/feature/5093566007214080
+    // this, is why container_el is given as an option. however, if none is given,
+    // it will still install a (performance limiting) non-passive handler.
+
+    prevent_default(event)
+    return roadtrip.goto(path, options)
+  }
+
+  let click_handler = (event) => {
+    // TODO: hopefully uglify merges the return statements into one statement. if not, merge them reducing the number of return statements
+    let w  = which(event), el, path, goto_path
+    if (
+        (w !== 1 && w !== 0) ||
+        (event.metaKey || event.ctrlKey || event.shiftKey) ||
+        (event.defaultPrevented) ||
+        !(el = get_internal_link_element(event))
+      ) return
+
+    // rebuild path
+    path = el.pathname + el.search + (el.hash || '')
+
+    // same page
+    goto_path = path
+
+    if (roadtrip.base) {
+      if (path.indexOf(roadtrip.base) === 0) path = path.substr(roadtrip.base.length)
+      if (goto_path === path) {
+        path = roadtrip.base + path
+        if (roadtrip.routes.some(route => route.matches(path))) goto_path = path
+        else return cancel_event_and_goto(event, path, {code: 404})
+      }
     }
 
-    let click_handler = (event) => {
-      // TODO: hopefully uglify merges the return statements into one statement. if not, merge them reducing the number of return statements
-      let w  = which(event), el, path, goto_path
-      if (
-          (w !== 1 && w !== 0) ||
-          (event.metaKey || event.ctrlKey || event.shiftKey) ||
-          (event.defaultPrevented)
-        ) return
-
-      // ensure target is a link
-      el = event.composed ? event.composedPath()[0] : event.target
-      while (el && el.nodeName !== 'A') el = el.parentNode
-
-      if (
-          !el ||
-          el.nodeName !== 'A' ||
-          el.hasAttribute('download') ||
-          el.getAttribute('rel') === 'external' ||
-          ~el.href.indexOf('mailto:') ||
-          el.target ||
-          !sameOrigin(el.href) // TODO: x-origin navigation function (which can do tracking or cancel the event)
-        ) return
-
-      // rebuild path
-      path = el.pathname + el.search + (el.hash || '')
-
-      // strip leading '/[drive letter]:' on NW.js on Windows
-      if (typeof process !== 'undefined' && path.match(/^\/[a-zA-Z]:\//)) {
-        path = path.replace(/^\/[a-zA-Z]:\//, '/')
-      }
-
-      // same page
-      goto_path = path
-
-      if (this.base) {
-        if (path.indexOf(this.base) === 0) path = path.substr(this.base.length)
-        if (goto_path === path) {
-          path = this.base + path
-          if (this.routes.some(route => route.matches(path))) goto_path = path
-          else return ev_goto(event, path, {code: 404})
-        }
-      }
-
-      // no match? allow navigation if this._404 isn't set
-      if (!this.routes.some(route => route.matches(goto_path)) && !this._404) {
-        return ev_goto(event, goto_path, {code: 404})
-      }
-
-      ev_goto(event, goto_path, {code: 200})
+    // no match? allow navigation if roadtrip._404 isn't set
+    if (!roadtrip.routes.some(route => route.matches(goto_path)) && !roadtrip._404) {
+      return cancel_event_and_goto(event, goto_path, {code: 404})
     }
 
-    let popstate_handler = (event) => {
-      const state = event.state
-      if (!state) return // hashchange, or otherwise outside roadtrip's control
-      const scroll = this.scrollHistory[ state.uid ]
+    cancel_event_and_goto(event, goto_path, {code: 200})
+  }
 
-      this._target = {
+  let popstate_handler = (event) => {
+    let state = event.state
+    let scroll = roadtrip.scrollHistory[state.uid]
+    if (state) {
+      // hashchange, or otherwise outside roadtrip's control
+      roadtrip._target = {
         href: location.href,
         scrollX: scroll.x || 0,
         scrollY: scroll.y || 0,
@@ -396,25 +412,32 @@ export default class RoadTrip {
         reject: noop
       }
 
-      this.goto_target(this._target)
-      this.currentID = event.state.uid
+      roadtrip.goto_target(roadtrip._target)
+      roadtrip.currentID = event.state.uid
     }
+  }
 
-    // watch history & clicks
-    // if chrome complains about document level listeners now being passive,
-    // (and as a result, preventDefault no longer works so navigation takes place anyway...)
-    // to fix this, pass `watchLinks` an element which frames your content
+  // watch history & clicks
+  // if chrome complains about document level listeners now being passive,
+  // (and as a result, preventDefault no longer works so navigation takes place anyway...)
+  // to fix this, pass `watchLinks` an element which frames your content
 
-    if (!container_el) container_el = win
-    on(container_el, 'click', click_handler, true)
-    on(container_el, 'touchstart', click_handler, true)
-    on(win, 'popstate', popstate_handler, {passive: true})
+  // there is another solution: when a like is clicked, have the click handler add
+  // click_hakdler to the link if it's not already added. use a WeakMap to check
+  // its existance. before doing this work, need to check to see if it's actually
+  // not a passive listener
+  //
+  //         -kenny 12-01-2020
 
-    // return a remove function
-    return () => {
-      off(container_el, 'click', click_handler, true)
-      off(container_el, 'touchstart', click_handler, true)
-      off(win, 'popstate', popstate_handler, {passive: true})
-    }
+  let is_passive = { passive: false, capture: true }
+  on(container_el, 'click', click_handler, is_passive)
+  on(container_el, 'touchstart', click_handler, is_passive)
+  on(win, 'popstate', popstate_handler, { passive: true })
+
+  // return a remove function
+  return () => {
+    off(container_el, 'click', click_handler, is_passive)
+    off(container_el, 'touchstart', click_handler, is_passive)
+    off(win, 'popstate', popstate_handler, { passive: true })
   }
 }
