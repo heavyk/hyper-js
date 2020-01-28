7512f6091f0aca11c72429bd8b3a35285aa49cb7
flames of love <mechanicofthesequence@gmail.com>
2020-01-28 15:53:07 -0700

    SUPPORT_ANCIENT flag added (to remove automatically compat code)

--- dom/request-animation-frame.js
+++ dom/request-animation-frame.js
@@ -1,21 +1,20 @@
 import { win } from './dom-base'
 
-var requestAnimationFrame = win.requestAnimationFrame ||
+var requestAnimationFrame = win.requestAnimationFrame || (SUPPORT_ANCIENT &&
   win.mozRequestAnimationFrame ||
   win.webkitRequestAnimationFrame || (function () {
-   /* rAF shim. Gist: https://gist.github.com/julianshapiro/9497513 */
-   var timeLast = 0
-   return function (callback) {
-     var timeCurrent = (new Date()).getTime(),
-       timeDelta
+    /* rAF shim. Gist: https://gist.github.com/julianshapiro/9497513 */
+    var timeLast = 0
+    return function (callback) {
+      var timeCurrent = (new Date()).getTime(), timeDelta
 
-     /* Dynamically set delay on a per-tick basis to match 60fps. */
-     /* Technique by Erik Moller. MIT license: https://gist.github.com/paulirish/1579671 */
-     timeDelta = Math.max(0, 16 - (timeCurrent - timeLast))
-     timeLast = timeCurrent + timeDelta
+      /* Dynamically set delay on a per-tick basis to match 60fps. */
+      /* Technique by Erik Moller. MIT license: https://gist.github.com/paulirish/1579671 */
+      timeDelta = Math.max(0, 16 - (timeCurrent - timeLast))
+      timeLast = timeCurrent + timeDelta
 
-     return setTimeout(function () { callback(timeCurrent + timeDelta); }, timeDelta)
-   }
- })()
+      return setTimeout(function () { callback(timeCurrent + timeDelta); }, timeDelta)
+    }
+  })())
 
 export default requestAnimationFrame
