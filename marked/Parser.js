fac288cbbe16977e1033f7128c32dd4157aeecbc
flames of love <mechanicofthesequence@gmail.com>
2020-02-19 02:06:03 -0700

    further improvements.

--- marked/Parser.js
+++ marked/Parser.js
@@ -16,7 +16,10 @@ export default function Parser (G, options = defaults) {
   let inline_output
   let inline_text_output
 
-  function parse (tokens) {
+  let next = () => token = tokens.pop()
+  let peek = () => tokens[tokens.length - 1] || 0
+
+  function parse (tokens, out = []) {
     inline_output = InlineLexer(tokens.links, options)
     // use an InlineLexer with a TextRenderer to extract pure text
     inline_text_output = InlineLexer(
@@ -25,7 +28,6 @@ export default function Parser (G, options = defaults) {
     )
     tokens = tokens.reverse()
 
-    let out = []
     while (next()) {
       out.push(tok())
     }
@@ -33,17 +35,7 @@ export default function Parser (G, options = defaults) {
     return out
   }
 
-  function next () {
-    return token = tokens.pop()
-  }
-
-  function peek () {
-    return tokens[tokens.length - 1] || 0
-  }
-
-  function parseText () {
-    let body = token.text
-
+  let parseText = (body = token.text) => {
     while (peek().type === 'text') {
       body += '\n' + next().text
     }
@@ -54,36 +46,27 @@ export default function Parser (G, options = defaults) {
   function tok () {
     let body
     let token = token
-    switch (token.type) {
-      case 'space': {
-        return ''
-      }
-      case 'hr': {
-        return renderer.hr()
-      }
-      case 'heading': {
-        return renderer.heading(
-          inline_output(token.text),
-          token.depth,
-          inline_text_output(token.text),
-          slugger
-        )
-      }
-      case 'code': {
-        return renderer.code(token.text,
-          token.lang,
-          token.escaped)
-      }
-      case 'link': {
-        return renderer.link[token.prefix](token.href, token.title, token.text)
-      }
-      case 'table': {
-        let header = []
-        let cells = []
-        let i, row, cell, j
-
+    let type = token.type
+    if (type === 'space') {
+      return ''
+    } else if (type === 'hr') {
+      return renderer.hr()
+    } else if (type === 'heading') {
+      return renderer.heading(
+        inline_output(token.text),
+        token.depth,
+        inline_text_output(token.text),
+        slugger
+      )
+    } else if (type === 'code') {
+      return renderer.code(token.text,
+        token.lang,
+        token.escaped)
+    } else if (type === 'link') {
+      return renderer.link[token.prefix](token.href, token.title, token.text)
+    } else if (type === 'table') {
+      return ((header = [], cells = [], i, j, row, cell = []) => {
         // header
-        cell = []
         for (i = 0; i < token.header.length; i++) {
           cell.push(renderer.tablecell(
             inline_output(token.header[i]),
@@ -95,7 +78,7 @@ export default function Parser (G, options = defaults) {
         for (i = 0; i < token.cells.length; i++) {
           row = token.cells[i]
 
-          cell = ''
+          cell = []
           for (j = 0; j < row.length; j++) {
             cell.push(
               renderer.tablecell(
@@ -109,18 +92,17 @@ export default function Parser (G, options = defaults) {
         }
 
         return renderer.table(header, cells)
-      }
-      case 'blockquote_start': {
-        body = []
-
+      })()
+    } else if (type === 'blockquote_start') {
+      return ((body = []) => {
         while (next().type !== 'blockquote_end') {
           body.push(tok())
         }
 
         return renderer.blockquote(body)
-      }
-      case 'list_start': {
-        body = []
+      })()
+    } else if (type === 'list_start') {
+      return ((body = []) => {
         const { ordered, start } = token
 
         while (next().type !== 'list_end') {
@@ -128,9 +110,9 @@ export default function Parser (G, options = defaults) {
         }
 
         return renderer.list(body, ordered, start)
-      }
-      case 'list_item_start': {
-        body = []
+      })()
+    } else if (type === 'list_item_start') {
+      return ((body = []) => {
         const loose = token.loose
         const checked = token.checked
         const task = token.task
@@ -152,16 +134,13 @@ export default function Parser (G, options = defaults) {
           )
         }
         return renderer.listitem(body, task, checked)
-      }
-      case 'paragraph': {
-        return renderer.paragraph(inline_output(token.text))
-      }
-      case 'text': {
-        return renderer.paragraph(parseText())
-      }
-      default: {
-        error('Token with "' + token.type + '" type was not found.')
-      }
+      })()
+    } else if (type === 'paragraph') {
+      return renderer.paragraph(inline_output(token.text))
+    } else if (type === 'text') {
+      return renderer.paragraph(parseText())
+    } else {
+      error('Token with "' + token.type + '" type was not found.')
     }
   }
 
